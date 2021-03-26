cbeedf2b7b72106430f258c87434cb953342147c
Your Name <csanchez112598@gmail.com>
2021-03-26 18:01:47 -0400

    added sql-ddl-design

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,20 +1,34 @@
 from flask import Flask, render_template, redirect, request, flash, session
 from surveys import satisfaction_survey
+from flask_debugtoolbar import DebugToolbarExtension
 app= Flask(__name__)
 app.config['SECRET_KEY']="carlos"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+
 
 @app.route('/start')
 def start():
+    # print("#############################################")
+    # print(session['RESPONSES'])
+    # print("#############################################")
+    
     return render_template("start.html", survey=satisfaction_survey)
 
 @app.route('/sesh', methods=["POST"])
 def set_session():
     session["RESPONSES"]=[]
+    print("#############################################")
+    print(session['RESPONSES'])
+    print("#############################################")
+    
     return redirect("/questions/0")
 
 
 @app.route('/questions/<int:q>')
 def show_questions(q):
+    print("#############################################")
+    print(session['RESPONSES'])
+    print("###############################f##############")
     RESPONSES=session['RESPONSES']
 
     if q!=len(RESPONSES):
@@ -28,11 +42,20 @@ def show_questions(q):
 
 @app.route('/thanks')
 def thank():
+    print("#############################################")
+    print(session['RESPONSES'])
+    print("#############################################")
     return render_template("thanks.html")
     
-@app.route('/answer')
-def handle_answer():    
-    answer= request.args['answer']
+
+@app.route('/answer', methods=["POST"])
+def handle_answer(): 
+    print("#############################################")
+    print(session['RESPONSES'])
+    print("#############################################")  
+
+    answer= request.form['answer']   
+    # answer= request.args['answer']
     RESPONSES=session['RESPONSES']
     RESPONSES.append(answer) 
     session['RESPONSES']=RESPONSES
@@ -41,3 +64,5 @@ def handle_answer():
         return redirect("/thanks")
     else:
         return redirect(f"/questions/{len(RESPONSES)}")
+     
+
