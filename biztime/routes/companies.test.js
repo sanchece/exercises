7cfc51761736d6ea755d9c1f1fe5857fa5aadcb5
carlos sanchez <sanchece98@gmail.com>
2021-08-25 00:51:03 -0400

    updated biztime with tests

--- biztime/routes/companies.test.js
+++ biztime/routes/companies.test.js
@@ -2,9 +2,8 @@ const request = require("supertest");
 const app = require("../app");
 const db = require("../db");
 
-
 beforeEach(async function () {
-    await db.query(`
+  await db.query(`
     CREATE TABLE IF NOT EXISTS companies (
       code text PRIMARY KEY,
       name text NOT NULL UNIQUE,
@@ -33,142 +32,112 @@ beforeEach(async function () {
                   ('skynet', 800, true, '2027-08-01', '2018-08-02')
            RETURNING id,comp_code`
   );
-
-  
 });
-
 
 afterEach(async function () {
   await db.query("DROP TABLE invoices");
   await db.query("DROP TABLE companies");
-
 });
 
 afterAll(async function () {
   // close db connection
   await db.end();
-  
-
 });
 
-
 describe("GET /", function () {
-
-    test("It should respond with array of companies", async function () {
-      const response = await request(app).get("/companies");
-      expect(response.body).toEqual({
-        "companies": [
-            {code: "google", name: "Google"},
-          {code: "skynet", name: "Skynet"},
-          
-        ]
-      });
-    })
-  
+  test("It should respond with array of companies", async function () {
+    const response = await request(app).get("/companies");
+    expect(response.body).toEqual({
+      companies: [
+        { code: "google", name: "Google" },
+        { code: "skynet", name: "Skynet" },
+      ],
+    });
   });
-  
-  
-describe("GET /skynet", function () {
+});
 
+describe("GET /skynet", function () {
   test("It return company info", async function () {
     const response = await request(app).get("/companies/skynet");
-    expect(response.body).toEqual(
-        {
-          "company": {
-            code: "skynet",
-            name: "Skynet",
-            description: "Artificial superintelligent neural network system",
-            invoices: [1, 2],
-          }
-        }
-    );
+    expect(response.body).toEqual({
+      company: {
+        code: "skynet",
+        name: "Skynet",
+        description: "Artificial superintelligent neural network system",
+        invoices: [1, 2],
+      },
+    });
   });
   test("It should return 404 for no-such-company", async function () {
     const response = await request(app).get("/companies/blargh");
     expect(response.status).toEqual(404);
-  })
   });
+});
 
-  describe("POST /", function () {
-
-    test("It should add company", async function () {
-      const response = await request(app)
-          .post("/companies")
-          .send({name: "TacoTime", description: "Yum!"});
-  
-      expect(response.body).toEqual(
-          {
-            "company": {
-              code: "tacotime",
-              name: "TacoTime",
-              description: "Yum!",
-            }
-          }
-      );
-    });
-
-    test("It should return 500 for conflict", async function () {
-      const response = await request(app)
-          .post("/companies")
-          .send({name: "Skynet", description: "Huh?"});
-  
-      expect(response.status).toEqual(500);
-    })
-  });
-
-  describe("PUT /", function () {
-
-    test("It should update company", async function () {
-      const response = await request(app)
-          .put("/companies/skynet")
-          .send({name: "SkynetEdit", description: "NewDescrip"});
-  
-      expect(response.body).toEqual(
-          {
-            "company": {
-              code: "skynet",
-              name: "SkynetEdit",
-              description: "NewDescrip",
-            }
-          }
-      );
-    });
-  
-    test("It should return 404 for no-such-comp", async function () {
-      const response = await request(app)
-          .put("/companies/blargh")
-          .send({name: "Blargh"});
-  
-      expect(response.status).toEqual(404);
-    });
-  
-    test("It should return 500 for missing data", async function () {
-      const response = await request(app)
-          .put("/companies/skynet")
-          .send({});
-  
-      expect(response.status).toEqual(500);
-    })
-  });
-  
-
-  
-
-describe("DELETE /", function () {
-
-  test("It should delete company", async function () {
+describe("POST /", function () {
+  test("It should add company", async function () {
     const response = await request(app)
-        .delete("/companies/skynet");
+      .post("/companies")
+      .send({ name: "TacoTime", description: "Yum!" });
 
-    expect(response.body).toEqual({"status": "deleted"});
+    expect(response.body).toEqual({
+      company: {
+        code: "tacotime",
+        name: "TacoTime",
+        description: "Yum!",
+      },
+    });
+  });
+
+  test("It should return 500 for conflict", async function () {
+    const response = await request(app)
+      .post("/companies")
+      .send({ name: "Skynet", description: "Huh?" });
+
+    expect(response.status).toEqual(500);
+  });
+});
+
+describe("PUT /", function () {
+  test("It should update company", async function () {
+    const response = await request(app)
+      .put("/companies/skynet")
+      .send({ name: "SkynetEdit", description: "NewDescrip" });
+
+    expect(response.body).toEqual({
+      company: {
+        code: "skynet",
+        name: "SkynetEdit",
+        description: "NewDescrip",
+      },
+    });
   });
 
   test("It should return 404 for no-such-comp", async function () {
     const response = await request(app)
+      .put("/companies/blargh")
+      .send({ name: "Blargh" });
 
-        .delete("/companies/blargh");
+    expect(response.status).toEqual(404);
+  });
+
+  test("It should return 500 for missing data", async function () {
+    const response = await request(app).put("/companies/skynet").send({});
+
+    expect(response.status).toEqual(500);
+  });
+});
+
+describe("DELETE /", function () {
+  test("It should delete company", async function () {
+    const response = await request(app).delete("/companies/skynet");
+
+    expect(response.body).toEqual({ status: "deleted" });
+  });
+
+  test("It should return 404 for no-such-comp", async function () {
+    const response = await request(app).delete("/companies/blargh");
 
     expect(response.status).toEqual(404);
   });
 });
-
