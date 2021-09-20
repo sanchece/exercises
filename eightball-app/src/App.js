51d06b6769d525f407727619e38a906229db5812
carlos sanchez <sanchece98@gmail.com>
2021-09-19 22:34:39 -0400

    added 8ball state exercise

---
+++ eightball-app/src/App.js
@@ -0,0 +1,13 @@
+import Eightball from "./Eightball"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Eightball />
+
+    </div>
+  );
+}
+
+export default App;
